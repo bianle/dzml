cadf4c05362c899ec5a19730f05a43099223f3a1
bianle <229371515@qq.com>
2016-02-16 16:07:22 +0800

    add chrome-extension

---
+++ chrome-extension/popup.js
@@ -0,0 +1,71 @@
+chrome.windows.getCurrent(function(win){
+  chrome.tabs.getSelected(function(tab){
+    var img,_width=200,_height=200;
+
+    //console.log(tab);
+    var url = tab.url;
+    jQuery('#lurl').text(url);
+    jQuery('#qrcode').qrcode({width:_width,height:_height,text:url});
+
+    if(tab.favIconUrl){
+      img = new Image();
+      img.src = tab.favIconUrl;
+      img.onload=function(){
+        var _w = img.width,_h = img.height
+        ,_x = 0 ,_y = 0
+        ;
+        if(_w && _h){
+          drawImg(img,(_width-_w)/2,(_height-_h)/2);
+        }
+      };
+    }
+
+    jQuery("#cutBtn").bind("click",function(){
+      cutUrl(url,function(data){
+        url = data;
+        jQuery("#lurl").text(url);
+      });
+    });
+
+
+    jQuery("#shareQzone").bind("click",function(){
+      doShare();
+    });
+
+  });
+});
+
+
+
+function cutUrl(url,cb){
+  jQuery.ajax({
+    url:'http://dzm.la/s/url/',
+    data:{u:url},
+    type:'post',
+    dataType:'text',
+    success:function(data){
+      cb("http://dzm.la/"+data);
+    },
+    error:function(e){
+      alert(e);
+    }
+  });
+}
+
+function doShare(){
+  cutUrl(jQuery('#lurl').text(),function(u){
+     window.open('http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?url='+encodeURIComponent(u));
+  });
+  return false;
+}
+
+
+//加入图片
+function drawImg(img,x,y){
+    var myCanvas = document.getElementsByTagName('canvas')
+        ,_canvas = myCanvas[0]
+        ,myctx = _canvas.getContext('2d')
+    ;
+    myctx.drawImage(img,x||0,y||0);
+
+}
